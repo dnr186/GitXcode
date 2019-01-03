92840b471c788d0b1402a4486e4d324716e8a791
Dorian <dnr186@googlemail.com>
2019-01-03 16:00:37 +0100

    Initial Commit

---
+++ GitXcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitXcode
+//
+//  Created by Dorian Riepe on 03.01.19.
+//  Copyright © 2019 Dorian Riepe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GitXcodeTests/GitXcodeTests.swift
@@ -0,0 +1,34 @@
+//
+//  GitXcodeTests.swift
+//  GitXcodeTests
+//
+//  Created by Dorian Riepe on 03.01.19.
+//  Copyright © 2019 Dorian Riepe. All rights reserved.
+//
+
+import XCTest
+@testable import GitXcode
+
+class GitXcodeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
